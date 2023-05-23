f7a87e59953b6c837397494f9123c12573b6e58e
Stywar <franco111988@gmail.com>
2023-05-22 21:46:55 -0500

    add file

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -5,12 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 09052022-v1.0.0 CI/CD in JENKINS JOB v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 22052023-v1.0.0 CI/CD with TRAVISCI JOB v1.0.0 AFORO255";
 });
 
 app.MapPost("/", () =>
@@ -19,5 +19,5 @@ app.MapPost("/", () =>
     return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
 });
 
-app.Run();
+app.Run($"http://0.0.0.0:{port}");
 
